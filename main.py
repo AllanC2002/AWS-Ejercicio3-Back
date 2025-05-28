0c8da0966206f758945bb44d3fc5c89e4dbac3bc
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-05-28 15:02:46 -0500

    fix
    123

--- main.py
+++ main.py
@@ -1,54 +1,45 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 
 app = Flask(__name__)
 
 @app.route("/clima", methods=["GET"])
 def clima():
-    # Ejemplo: clima en Quito (latitud y longitud)
-    lat = -0.22
-    lon = -78
+    lat = request.args.get("lat")
+    lon = request.args.get("lon")
     url = f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true'
-
-    # Hacemos la petición a la API externa
     response = requests.get(url)
 
-    # Verificamos si fue exitosa
     if response.status_code == 200:
         data = response.json()
         temperature = data["current_weather"]["temperature"]
         return jsonify(temperature)
     else:
         return jsonify({'error': 'No se pudo obtener el clima'}), 500
-    
+
 @app.route("/pais", methods=["GET"])
 def pais():
-    name = "ecuador"
+    name = request.args.get("name")
     url = f'https://restcountries.com/v3.1/name/{name}'
-
     response = requests.get(url)
 
     if response.status_code == 200:
-        data = response.json()[0]  # la respuesta es una lista
+        data = response.json()[0]
         capital = data['capital'][0]
         lat, lon = data['capitalInfo']['latlng']
-        return jsonify(capital,lat,lon)
+        return jsonify([capital, lat, lon])
     else:
         return jsonify({'error': 'No se pudo obtener el país'}), 500
-
 
 @app.route("/consejo", methods=["GET"])
 def consejo():
     response = requests.get('https://api.adviceslip.com/advice')
     if response.status_code == 200:
-        data = response.json() 
+        data = response.json()
         consejo = data['slip']['advice']
         return jsonify({'consejo': consejo})
     else:
         return jsonify({'error': 'No se pudo obtener el consejo'}), 500
-
-
-
 
 if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
